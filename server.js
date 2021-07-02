09451d986f37f2552d5fb259ab3c997de2859af7
eved4 <73189965+eved4@users.noreply.github.com>
2021-07-02 17:05:49 +0100

    second commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, './')));
+
+app.get('/', (request, response) => {
+  response.sendFile(path.join(__dirname, './index.html'));
+});
+
+app.get('/', (request, response) => {
+  response.sendFile(path.join(__dirname, './index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Express server listening on port ${port}!`);
+});
