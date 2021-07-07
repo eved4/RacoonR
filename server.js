6473e6521a2a80f3c18b4fa31c1415a070cecba3
eved4 <ucabeda@ucl.ac.uk>
2021-07-07 13:55:01 +0100

    changes

--- server.js
+++ server.js
@@ -1,18 +1,76 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
-
+User = require('./models/user');
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, './')));
+// static files
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'));
+app.use('/js', express.static(__dirname + 'public/js'));
+app.use('/img', express.static(__dirname + 'public/img'));
 
-app.get('/', (request, response) => {
-  response.sendFile(path.join(__dirname, './index.html'));
+// set views
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.get('', (req, res) => {
+  res.render('index');
 });
 
-app.get('/', (request, response) => {
-  response.sendFile(path.join(__dirname, './index.html'));
+app.get('/browse', (req, res) => {
+  res.render('browse');
+});
+
+app.get('/account', (req, res) => {
+  res.render('account');
+});
+
+app.get('/additem', (req, res) => {
+  res.render('additem');
+});
+
+app.get('/browsewanted', (req, res) => {
+  res.render('browsewanted');
+});
+
+app.get('/communities', (req, res) => {
+  res.render('communities');
+});
+
+app.get('/community', (req, res) => {
+  res.render('community');
+});
+
+app.get('/communityform', (req, res) => {
+  res.render('communityform');
+});
+
+app.get('/eventpage', (req, res) => {
+  res.render('eventpage');
+});
+
+app.get('/events', (req, res) => {
+  res.render('events');
+});
+
+app.get('/indexloggedin', (req, res) => {
+  res.render('indexloggedin');
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.get('/offeritem', (req, res) => {
+  res.render('offeritem');
+});
+
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
+app.get('/wanteditem', (req, res) => {
+  res.render('wanteditem');
 });
 
 app.listen(port, () => {
---
+++ test/connection.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+// connect to mongodb
+
+mongoose.connect('mongodb://localhost/testaroo');
+
+
+
+mongoose.connection.once('open', function(){
+console.log('connection has been made, now make fireworks...')
+}).on('error', function(error){
+    console.log('connection error', error);
+}
+
+})
